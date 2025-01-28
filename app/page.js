b05a20b58ace8290275f263d05a74c8a4bbb672b
Gabrielbielvl <gabrielbirges5@gmail.com>
2025-01-27 21:15:27 -0300

    primeiro commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1>Enviando para Github</h1>
+      <p>Atividade finalizada!</p>
+
+    </div>
+  );
+}

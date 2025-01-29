cd50f3ef5ab03dea2209b2185540bd9f81a0b468
Gabrielbielvl <gabrielbirges5@gmail.com>
2025-01-28 22:10:30 -0300

    aula de css
    28/01/25

---
+++ app/components/Menu.js
@@ -0,0 +1,17 @@
+import "./Menu.css"
+
+export default function Menu(){
+    return(
+        <div className="menu"> 
+            
+            <h1> bielvl</h1>
+
+            <div>
+            <a href="#">Inicio</a>
+            <a href="#">Produtos</a>
+            <a href="#">Contatos</a>
+            </div>
+
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,29 @@
 import Image from "next/image";
+import Menu from "./components/Menu";
+import Produto from "./components/Produto";
 
 export default function Home() {
   return (
     <div>
 
+      <Menu/>
+
+      
       <h1>Enviando para Github</h1>
       <p>Atividade finalizada!</p>
+
+      <hr/>
+
+      <h2>Produtinhux</h2>
+
+      <div className="flex justify-around">
+
+      <Produto nome="vilao começo filme" preco="1.000.000,00" estrela="⭐⭐⭐" botao="compra"/>
+      <Produto nome="vilao meio" preco="100.000" estrela="⭐⭐" botao="PROMOCAO" />
+      <Produto nome="vilao fim" preco="1.000" estrela="⭐" botao="pendente"/>
+      <Produto nome="vilao gratis" preco="00,00" estrela="⭐" botao="pendente"/>
+
+      </div>
 
     </div>
   );
